979684f9479078f06b3ceae2452c3937392d3a70
TheHolsic <morozyukmisha3003@gmail.com>
2024-03-09 18:42:09 +0300

    Update player

--- player.py
+++ player.py
@@ -12,30 +12,23 @@ class Player:
     def draw(self):
         pg.draw.rect(self.screen, 'black', (self.x, self.y), 10)
 
-    def move (self, x1_change, y1_change, key):
-        game_over = False
-        x1_change = 0
-        y1_change = 0
-        while not game_over:
-                if type == pg.KEYDOWN: #Добавляем считывание направления
-                                            #движений с клавиатуры.
-                    if key == pg.K_LEFT:
-                        self.x1_change = -10 #Указываем шаг изменения положения змейки
-                                            #в 10 пикселей.
-                        self.y1_change = 0
-                    elif key == pg.K_RIGHT:
-                        self.x1_change = 10
-                        self.y1_change = 0
-                    elif key == pg.K_UP:
-                        self.y1_change = -10
-                        self.x1_change = 0
-                    elif key == pg.K_DOWN:
-                        self.y1_change = 10
-                        self.x1_change = 0
-        self.x += x1_change #Записываем новое значение положения змейки по оси х.
-        self.y += y1_change #Записываем новое значение положения змейки по оси y.
-        if self.x >= dis_width or self.x < 0 or self.y >= dis_height or self.y < 0:
-            game_over = True
+    def move (self, event):
+        if event.type == pg.KEYDPWN:
+            if event.key == pg.K_LEFT:
+                self.x1_change = -10 #Указываем шаг изменения положения змейки в 10 пикселей.
+                self.y1_change = 0
+            elif event.key == pg.K_RIGHT:
+                self.x1_change = 10
+                self.y1_change = 0
+            elif event.key == pg.K_UP:
+                self.y1_change = -10
+                self.x1_change = 0
+            elif event.key == pg.K_DOWN:
+                self.y1_change = 10
+                self.x1_change = 0
+        self.x += self.x1_change #Записываем новое значение положения змейки по оси х.
+        self.y += self.y1_change #Записываем новое значение положения змейки по оси y.
+
 
     def update(self):
         pass
